44584128330b5b311f39e49e33491a799f7afe66
rahul-apple <rahulthazhuthala@gmail.com>
2015-09-09 15:15:03 +0530

    Initial Commit

---
+++ KeyChain_Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeyChain_Sample
+//
+//  Created by RAHUL'S MAC MINI on 09/09/15.
+//  Copyright (c) 2015 iDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KeyChain_Sample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KeyChain_Sample
+//
+//  Created by RAHUL'S MAC MINI on 09/09/15.
+//  Copyright (c) 2015 iDev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
